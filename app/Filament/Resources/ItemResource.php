007c946b194f433075875fe0e955f37a8195261f
Igor Rjabinin <igor.rjabinin@gmail.com>
2024-05-30 14:38:14 +0200

    admin add ItemResource

---
+++ app/Filament/Resources/ItemResource.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ItemResource\Pages;
+use App\Filament\Resources\ItemResource\RelationManagers;
+use App\Models\Item;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class ItemResource extends Resource
+{
+    protected static ?string $model = Item::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('x')
+                    ->numeric(),
+                Forms\Components\TextInput::make('y')
+                    ->numeric(),
+                Forms\Components\TextInput::make('span_x')
+                    ->numeric(),
+                Forms\Components\TextInput::make('span_y')
+                    ->numeric(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('id')
+                    ->label('ID')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('x')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('y')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('span_x')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('span_y')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ManageItems::route('/'),
+        ];
+    }
+}
