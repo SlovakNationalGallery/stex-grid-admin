aa8a334d57645a774d3bd7b9a704b954f3a08ff2
Igor Rjabinin <igor.rjabinin@gmail.com>
2024-06-18 13:29:18 +0200

    add spatie/laravel-responsecache

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,8 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->append(\App\Http\Middleware\Localize::class);
+        $middleware->append(\Spatie\ResponseCache\Middlewares\CacheResponse::class);
+
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
